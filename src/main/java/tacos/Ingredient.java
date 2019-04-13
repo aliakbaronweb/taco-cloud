e8f0d91f67ff5a28f17a515af1ba1c497edf7b75
Ali Akbar <aliakbaronweb@yahoo.com>
2019-04-13 20:10:37 +0430

    add validation

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,18 +1,18 @@
 package tacos;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class Ingredient {
-	
-	private String id;
-	private String name;
-	private Type type;
-	
-	public static enum Type {
-		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE;
-	}
+  
+  private final String id;
+  private final String name;
+  private final Type type;
+  
+  public static enum Type {
+    WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+  }
 
 }
